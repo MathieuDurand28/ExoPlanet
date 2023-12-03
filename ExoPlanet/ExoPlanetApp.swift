03e9406afc835d2a90e315db397498f408dc7fce
Mathieu Durand <32414550+MathieuDurand28@users.noreply.github.com>
2023-12-03 15:52:14 +0100

    First commit

---
+++ ExoPlanet/ExoPlanetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExoPlanetApp.swift
+//  ExoPlanet
+//
+//  Created by Mathieu Durand on 30/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExoPlanetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
